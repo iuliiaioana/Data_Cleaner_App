64dcf579b22f6b89f3f6b0c1bf4034ce2b9a2ca7
iuliiaioana <52716157+iuliiaioana@users.noreply.github.com>
2021-05-23 23:43:49 +0300

    Outliers delete+replace iqr

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
 import pandas as pd
-
 from actions.alter_data_action.drop import Drop
 from actions.alter_data_action.duplicate import Duplicate
 from actions.alter_data_action.fill_na import FillNa
-from actions.data_visualisation_action.data_visualisation import \
-    DataVisualisation
+from actions.alter_data_action.outliers import Outliers
+from actions.data_visualisation_action.data_visualisation import DataVisualisation
 from actions.get_data_action.get_data import Data
 from actions.upload_file_action.upload_file import UploadFile
 
@@ -12,7 +11,7 @@ pd.set_option('display.max_columns', None)
 pd.set_option('display.max_rows', None)
 
 if __name__ == '__main__':
-    f = UploadFile('upload_files/shootings.csv')
+    f = UploadFile('fisiereDeUpload/shootings.csv')
     data = Data(data_frame=f.get_data_from_file())
     data_frame = data.get_data()
 
@@ -26,5 +25,15 @@ if __name__ == '__main__':
     # # print(test_duplicate.duplicate_columns_selection('name'))
     # print(test_duplicate.duplicate_columns_selection_last('name'))
 
-    test_drop = Drop(data_frame)
-    print(test_drop.drop_columns('age'))
+    # test_drop = Drop(data_frame)
+    # print(test_drop.drop_columns('age'))
+
+    test_outlier= Outliers(data_frame)
+    # print(test_outlier.delete_outliers_zscore(['age']))
+    # print(test_outlier.delete_outliers_interquartile(['age']))
+    print(test_outlier.replace_ouliers_interquartile(['age']))
+
+    # print()
+    # d=DataVisualisation(data_frame)
+    # d.get_info()
+
