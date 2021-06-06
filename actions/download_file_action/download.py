2b8e0325cb14da3207bbfdf64edd650e3d9afa4d
irina.botez <irina.botez@eaudeweb.ro>
2021-06-06 23:46:02 +0300

    Fixed paths for download

--- actions/download_file_action/download.py
+++ actions/download_file_action/download.py
@@ -17,18 +17,17 @@ class DownloadFile:
         self.df = self.df.to_json(self.filepath, orient='split')
 
     def start(self):
-        filename = input('Give name of the proceed file:')
-        path = os.path.abspath(os.getcwd())
-        path += "\\fisiereDownload\\"
-        extension = input('Choose from 1)csv 2)excel 3)json')
+        filename = input('Give name of the proceed file: ')
+        path = os.path.join(os.path.abspath(os.getcwd()), 'download_files')
+        extension = input('Choose from 1)csv 2)excel 3)json:\n')
         if extension == '1':
-            self.filepath = path + filename + '.csv'
+            self.filepath = os.path.join(path, filename + '.csv')
             self.write_csv()
         elif extension == '2':
-            self.filepath = path + filename + '.xlsx'
+            self.filepath = os.path.join(path, filename + '.xlsx')
             self.write_excel()
         elif extension == '3':
-            self.filepath = path + filename + '.json'
+            self.filepath = os.path.join(path, filename + '.json')
             self.write_json()
         else:
             print('Please give a valid option')
